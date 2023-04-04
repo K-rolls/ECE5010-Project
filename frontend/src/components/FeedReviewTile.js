77130403ac8bd2f2c482414d01c76f43b3668ee2
bobbylush02 <rwlush@mun.ca>
2023-04-03 23:31:51 -0230

    -FeedReviewTile & Review Modal Co-authored-by: Kirkland Keeping <KirklandKeeping@users.noreply.github.com>

--- frontend/src/components/FeedReviewTile.js
+++ frontend/src/components/FeedReviewTile.js
@@ -1,47 +1,139 @@
 import {
-    Image, Text, Button, Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
+  Image,
+  Text,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Divider,
+  Avatar,
 } from "@chakra-ui/react";
+import { Dialog } from "@headlessui/react";
 import Link from "next/link";
+import { useState } from "react";
+import CustomButton from "./CustomButton";
 
 function FeedReviewTile({ album, review, rating, username }) {
-    const handleAlbumClick = (album) => {
-        console.log("Album clicked:", album);
-    };
+  const handleAlbumClick = (album) => {
+    console.log("Album clicked:", album);
+  };
 
-    return (
-        <div className="album-tile flex-col p-4 space-y-2 bg-background rounded-xl border-4 snap-y min-w-[300px] max-h-[250px] border-mainblue">
-            <div className="flex flex-row space-x-4">
-                <Link href={`/Album/?id=${album.id}`}>
-                    <button
-                        // disabled={loading}
-                        className="border-[4px] border-white rounded-md"
-                    >
-                        <Image src={album.image} alt={album.name} boxSize="167px" objectFit="cover" />
-                    </button>
-                </Link>
-                <div className="flex flex-col space-y-2 text-white max-w-[500px]">
-                    <div className="flex flex-row overflow-hidden space-x-2">
-                        <img src="/favicon-32x32.png" alt="profile picture" className="w-8 h-8 rounded-full" />
-                        <Text className="font-permanent-marker">{username}</Text>
-                        <Text className="flex font-permanent-marker justify-end">{rating}★</Text>
-                    </div>
-                    {review && (
-                        <div className="border-2 p-3 rounded-md border-accentlavender text-black bg-white w-[200px]">
-                            <Text>{review}</Text>
-                        </div>
-                    )}
+  const [isOpen, setIsOpen] = useState(false);
 
-                </div>
+  const openReviewModal = () => {};
 
+  return (
+    <div className="album-tile flex-col p-4 space-y-2 bg-background rounded-xl border-4 snap-y min-w-[300px] max-h-[250px] border-mainblue">
+      <div className="flex flex-row space-x-4">
+        <Link href={`/Album/?id=${album.id}`}>
+          <button
+            // disabled={loading}
+            className="border-[4px] border-white rounded-md"
+          >
+            <Image
+              src={album.image}
+              alt={album.name}
+              boxSize="167px"
+              objectFit="cover"
+            />
+          </button>
+        </Link>
+        <div className="flex flex-col space-y-4 text-white text-xl max-w-[500px] justify-center items-center">
+          <div className="flex flex-row overflow-hidden space-x-2">
+            <img
+              src="/favicon-32x32.png"
+              alt="profile picture"
+              className="w-8 h-8 rounded-full"
+            />
+            <div className="font-permanent-marker text-clip w-[95px] whitespace-nowrap overflow-hidden">
+              {username}
             </div>
+            <Text className="flex font-permanent-marker justify-end">
+              {rating}★
+            </Text>
+          </div>
+          {review && (
+            <CustomButton text="Read Review" onClick={() => setIsOpen(true)} />
+          )}
         </div>
-    );
+        <Dialog
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          className="relative z-50"
+        >
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+          <div className="fixed inset-0 flex items-center justify-center p-4">
+            <Dialog.Panel className="text-white min-w-[500px] min-h-[200px] bg-black border-accentlavender border-[7px] p-8 overflow-auto text-justify rounded-2xl">
+              <Dialog.Title>
+                <div className="text-3xl flex flex-col font-permanent-marker">
+                  <div className="flex flex-row justify-center">
+                    <div className="font-permanent-marker">{album.name}</div>
+                  </div>
+                  <div className="py-4">
+                    <Divider orientation="horizontal" />
+                  </div>
+                </div>
+              </Dialog.Title>
+              <Dialog.Description>
+                <Text className="flex text-xl max-w-[550px]">{review}</Text>
+              </Dialog.Description>
+              <Dialog.Title>
+                <div className="text-3xl font-permanent-marker flex flex-col space-y-2 text-white pt-6">
+                  <div className="flex flex-row justify-between overflow-hidden">
+                    <Text className="pt-1 flex font-permanent-marker">
+                      {rating}★
+                    </Text>
+                    <div className="flex flex-row space-x-4">
+                      <Text className="pt-1 font-permanent-marker">
+                        {username}
+                      </Text>
+                      <Avatar
+                        size="md"
+                        src="/favicon-32x32.png"
+                        alt="profile picture"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </Dialog.Title>
+              {/* ... */}
+            </Dialog.Panel>
+          </div>
+        </Dialog>
+      </div>
+    </div>
+  );
 }
 
 export default FeedReviewTile;
+
+{
+  /* <Modal isOpen={isOpen} onClose={onClose} isCentered>
+  <ModalOverlay backdropFilter="blur(5px)" />
+  <ModalContent>
+    <ModalHeader>
+      <div className="flex flex-col font-permanent-marker">
+        <div className="flex flex-row">
+          <div className="font-permanent-marker">{album.name}</div>
+        </div>
+        <div className="flex flex-row">
+          <div className="font-permanent-marker text-clip w-[95px] whitespace-nowrap overflow-hidden">
+            {username}
+          </div>
+          <Text className="flex font-permanent-marker justify-end">
+            {rating}★
+          </Text>
+        </div>
+      </div>
+    </ModalHeader>
+    <ModalCloseButton />
+    <ModalBody>{review}</ModalBody>
+    <ModalFooter> <Button onClick={onClose}>Close</Button> </ModalFooter>
+  </ModalContent>
+</Modal> */
+}
